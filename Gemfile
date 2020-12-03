f58e15bd95a61c9e5d85f04ce8ec2b2706eb41ef
krpk1900 <krpk1900@gmail.com>
2020-12-03 11:31:24 +0900

    [Add] POROで仮のTaskモデルスペックを追加

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'sorcery'
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'pry-rails'
   gem 'rspec-rails', '~> 4.0.1'
   gem 'factory_bot_rails'
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    before do
+      @user = User.create(id: 1, email: 'test1@example.com', password: 'password', password_confirmation: 'password')
+      @task = @user.tasks.build(id: 1, title: 'Tiele', content: 'Content', status: 0, deadline: '2020-12-31')
+    end
+    it 'is valid with all attributes' do
+
+      binding.pry
+
+      expect(@task).to_not be_vaild
+    end
+    it 'is invalid without title' do end
+    it 'is invalid without status' do end
+    it 'is invalid with a duplicate title' do end
+    it 'is valid with another title' do end
+  end
 end
